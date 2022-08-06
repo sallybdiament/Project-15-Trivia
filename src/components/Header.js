8eecf402c46b04b0a67071f27103894ae2aedcb1
Anderson Correa <andersonfpcorrea@gmail.com>
2022-08-05 23:49:42 -0300

    requisito 5 concluído

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { photo, name, score } = this.props;
+    console.log(photo);
+    return (
+      <header>
+        <div>
+          <img src={ photo } alt={ name } data-testid="header-profile-picture" />
+          <p data-testid="header-player-name">{ name }</p>
+        </div>
+        <p data-testid="header-score">{ score }</p>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  photo: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+Header.defaultProps = {
+  photo: 'https://www.gravatar.com/avatar/c19ad9dbaf91c5533605fbf985177ccc',
+};
+
+const mapStateToProps = (state) => ({
+  photo: state.player.gravatarEmail,
+  name: state.player.name,
+  score: state.player.score,
+});
+
+export default connect(mapStateToProps)(Header);
---
+++ src/redux/actions/index.js
@@ -0,0 +1,6 @@
+export const STORE_PHOTO = 'STORE_PHOTO';
+
+export const storePhoto = (data) => ({
+  type: STORE_PHOTO,
+  payload: data,
+});

d894106f3270eda6136633ed8c8857003213c535
Anderson Correa <andersonfpcorrea@gmail.com>
2022-08-09 00:39:56 -0300

    req 7 (sem ?. e eslint OK)

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,10 +4,9 @@ import Header from '../components/Header';
 
 class Game extends React.Component {
   state = {
-      questionList: [],
-      loaded: false,
-    }
-  
+    questionList: [],
+    loaded: false,
+  }
 
   componentDidMount() {
     this.fetchQuestions();
@@ -30,10 +29,16 @@ class Game extends React.Component {
     const { questionList } = this.state;
     if (!questionList[0]) return null;
     const correct = (
-      <button data-answer="correct" key="correct" type="button" data-testid="correct-answer">
+      <button
+        data-answer="correct"
+        key="correct"
+        type="button"
+        data-testid="correct-answer"
+      >
         {questionList[0].correct_answer}
-      </button>);
-     
+      </button>
+    );
+
     const incorrects = questionList[0].incorrect_answers.map(
       (resp, index) => (
         <button
@@ -47,35 +52,35 @@ class Game extends React.Component {
     );
     const answersArray = [correct, ...incorrects];
     const answerArray2 = [...incorrects, correct];
-    const random = Math.random(); 
-    
-    if (random < 0.55) {
+    const random = Math.random();
+    const randomLimit = 0.55;
+
+    if (random < randomLimit) {
       return answersArray;
-    } else {
-      return answerArray2
     }
+    return answerArray2;
   }
 
-  handleClick = ({target}) => {
-   if (!target.type) return null;
-   console.log(target.parentElement);
-   const parent = target.parentElement;
-   console.log(Array.from(parent.children));
-   const children = Array.from(parent.children);
+  handleClick = ({ target }) => {
+    if (!target.type) return null;
+    console.log(target.parentElement);
+    const parent = target.parentElement;
+    console.log(Array.from(parent.children));
+    const children = Array.from(parent.children);
 
-   children.forEach((btn) => {
-    if (btn.dataset.answer === 'correct') {
-      btn.style.border = '3px solid rgb(6 ,240, 15)'
-     } else {
-      btn.style.border = '3px solid rgb(255, 0, 0)'
-     } 
-   })
+    children.forEach((btn) => {
+      if (btn.dataset.answer === 'correct') {
+        btn.style.border = '3px solid rgb(6 ,240, 15)';
+      } else {
+        btn.style.border = '3px solid rgb(255, 0, 0)';
+      }
+    });
   }
 
   renderText = (title) => {
     const { questionList } = this.state;
     if (questionList[0]) return questionList[0][`${title}`];
-    else return null;
+    return null;
   }
 
   render() {
@@ -85,11 +90,18 @@ class Game extends React.Component {
         <Header />
         <p data-testid="question-category">{ this.renderText('category') }</p>
         <p data-testid="question-text">{ this.renderText('question') }</p>
-        <div type="div" data-testid="answer-options" onClick={ this.handleClick }>
+        <div
+          role="presentation"
+          type="div"
+          data-testid="answer-options"
+          onClick={ this.handleClick }
+        >
           { loaded && this.renderQuestions() }
         </div>
-    </>
-)}};
+      </>
+    );
+  }
+}
 
 Game.propTypes = {
   history: PropTypes.objectOf(PropTypes.any),

b14c0ba1d17ed8524e0699248e327ba40d0524e1
Sally Diament <sally.barzilai@gmail.com>
2022-11-16 21:28:28 -0300

    Updates

---
+++ precache-manifest.37572037d4c5bede4a77d0d5d9cbf7b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f80dfb9547deae6a41d9522bdc4e554",
+    "url": "/Project-15-Trivia/index.html"
+  },
+  {
+    "revision": "b5c8c2df39a3c3040a7f",
+    "url": "/Project-15-Trivia/static/css/main.ecc843ec.chunk.css"
+  },
+  {
+    "revision": "7427b3fac13fee74b63b",
+    "url": "/Project-15-Trivia/static/js/2.665ba02f.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/Project-15-Trivia/static/js/2.665ba02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5c8c2df39a3c3040a7f",
+    "url": "/Project-15-Trivia/static/js/main.15e0fd4c.chunk.js"
+  },
+  {
+    "revision": "717494eee50df1507683",
+    "url": "/Project-15-Trivia/static/js/runtime-main.ef1e2f7e.js"
+  },
+  {
+    "revision": "eead45b119ede98245e6b644131aefaa",
+    "url": "/Project-15-Trivia/static/media/fundo login smarthphone.eead45b1.png"
+  },
+  {
+    "revision": "089d8efd2b7a81c5a76e494d80a6fa53",
+    "url": "/Project-15-Trivia/static/media/fundoLightMobile.089d8efd.png"
+  },
+  {
+    "revision": "ee752941185a9a1c47d5009fb1baca86",
+    "url": "/Project-15-Trivia/static/media/loginBackgroundLargeScreen.ee752941.png"
+  },
+  {
+    "revision": "e0674fb48d6afc591d0b07fd27797261",
+    "url": "/Project-15-Trivia/static/media/logo trivia sem tracado.e0674fb4.png"
+  }
+]);
